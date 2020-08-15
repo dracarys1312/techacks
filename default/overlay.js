9b0616d18467ab1b8144019239ec360c5f9c30f0
Athos <dracarys1312@gmail.com>
2020-08-15 21:27:59 +0700

    Basic filter Bar and modal form

--- default/overlay.js
+++ default/overlay.js
@@ -1,6 +1,11 @@
-$(document).ready(function () {
-    $('#sidebarCollapse').on('click', function () {
-        $('#sidebar').toggleClass('active');
-        $(this).toggleClass('active');
-    });
-});
+function openNav() {
+  document.getElementById("filterBar").style.width = "250px";
+  document.getElementById("main").style.marginLeft = "250px";
+  document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
+}
+
+function closeNav() {
+  document.getElementById("filterBar").style.width = "0";
+  document.getElementById("main").style.marginLeft= "0";
+  document.body.style.backgroundColor = "#EBEF16";
+}
