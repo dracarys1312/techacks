f0e06d467156c38810e3a1aa4ab8b2f3d52a170e
Athos <dracarys1312@gmail.com>
2020-08-16 20:20:08 +0700

    Adding `build` to source control

---
+++ client/build/precache-manifest.1c25b4a2e457a08f8db294b120b77512.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "22852960d305ec77752fe24a5dca9c1b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3712caf4cd9e5c6b7634",
+    "url": "/static/css/main.14af520f.chunk.css"
+  },
+  {
+    "revision": "f4962cc3a96de6c4315b",
+    "url": "/static/js/2.9775c59c.chunk.js"
+  },
+  {
+    "revision": "eae3a7fd0ed99f94bc01fcd0b4491339",
+    "url": "/static/js/2.9775c59c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3712caf4cd9e5c6b7634",
+    "url": "/static/js/main.532573db.chunk.js"
+  },
+  {
+    "revision": "0e35831b56fb2c044e12",
+    "url": "/static/js/runtime-main.98a2b520.js"
+  }
+]);
